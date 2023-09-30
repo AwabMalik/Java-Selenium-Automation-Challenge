4320c8426661758f82b258a1a16bfb391c095879
AwabMalik <146194445+AwabMalik@users.noreply.github.com>
2023-09-30 11:51:56 +0500

    Update ProductDetailPage.java

--- SmallWorld Automation Challenge/src/test/java/Login/ProductDetailPage.java
+++ SmallWorld Automation Challenge/src/test/java/Login/ProductDetailPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 public class ProductDetailPage {
     private WebDriver driver;
 
-    // Web elements
+    
     private By productTitleLabel = By.className("inventory_details_name");
     private By addToCartButton = By.cssSelector("button[id^='add-to-cart']");
 
@@ -15,7 +15,7 @@ public class ProductDetailPage {
         this.driver = driver;
     }
 
-    // Actions
+    
     public String getProductTitle() {
         return driver.findElement(productTitleLabel).getText();
     }
