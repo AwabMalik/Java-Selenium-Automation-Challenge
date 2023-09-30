f5988c1b287068c5d903b1d40d77b9024888059d
AwabMalik <146194445+AwabMalik@users.noreply.github.com>
2023-09-30 11:50:48 +0500

    Update ConfirmationPage.java

--- SmallWorld Automation Challenge/src/test/java/Login/ConfirmationPage.java
+++ SmallWorld Automation Challenge/src/test/java/Login/ConfirmationPage.java
@@ -8,13 +8,13 @@ import org.openqa.selenium.WebElement;
 public class ConfirmationPage {
     private WebDriver driver;
 
-    // Web elements
+    
     private By completeMessageLabel = By.cssSelector("h2[class='complete-header']");
 
     public ConfirmationPage(WebDriver driver) {
         this.driver = driver;
     }
-    // Action to get the complete message
+    
     public String getCompleteMessage() {
         return driver.findElement(completeMessageLabel).getText();
     }
